48e1ec2f7f933a51231719b4b74efbc86c6269db
Zkera <108960099+Zkera@users.noreply.github.com>
2024-09-17 18:36:50 +0300

    Update 3_2.c

--- c/lab2/3_2.c
+++ c/lab2/3_2.c
@@ -8,6 +8,6 @@ int main()
     f = (n < 0 && m > 0) ? (m + pow(n,2)) : 
         (n < 0 && m <= 0) ? (n * 2 + m):
         (m + 1);
-    printf("F(m,n) = %.02lf", f);
+    printf("F(m,n) = %.2lf", f);
     return 0;
 }
