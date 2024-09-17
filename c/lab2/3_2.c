1fc84d82e4db8971f55b2bc58afc1676435acff8
Zkera <108960099+Zkera@users.noreply.github.com>
2024-09-17 18:35:56 +0300

    Create 3_2.c

---
+++ c/lab2/3_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double m, n, f;
+    scanf("%lf%lf", &m,&n);
+    f = (n < 0 && m > 0) ? (m + pow(n,2)) : 
+        (n < 0 && m <= 0) ? (n * 2 + m):
+        (m + 1);
+    printf("F(m,n) = %.02lf", f);
+    return 0;
+}
